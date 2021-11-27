72d97c98017afd18e4a41de35ffdcdd7cb74a2ea
Xpucmemo <56083690+Xpucmemo@users.noreply.github.com>
2021-11-27 22:57:32 +0200

    Hotfixed Navigation Bar
    lubo e gei

--- js/main.js
+++ js/main.js
@@ -2,8 +2,3 @@
 $(function () {
     $("#nav-placeholder").load("./Navigation/nav.html");
 });
-
-$(".menu-toggle-btn").click(function () {
-    $(this).toggleClass("fa-times");
-    $(".navigation-menu").toggleClass("active");
-});
